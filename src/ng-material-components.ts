825c3e0897bd2db4ba29f35c2e5be03c7a42b0b2
Orlando Quero <orlaqp@binaryways.com>
2017-05-09 17:11:10 -0400

    removed some unnecesary files from demo

--- src/ng-material-components.ts
+++ src/ng-material-components.ts
@@ -2,3 +2,4 @@
 export { SumService } from './services/sum-service';
 export { ArithmeticModule } from './modules/arithmetic-module';
 export { MaterialUserInterfaceModule } from './modules/user-interface/material-user-interface.module';
+export { MaterialFormsModule } from './modules/forms/material-forms.module';
