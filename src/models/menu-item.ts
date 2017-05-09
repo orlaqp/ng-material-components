6c1906f4a3fa3b6286b35b4a25c5fe54699596c5
Orlando Quero <orlaqp@gmail.com>
2017-05-09 08:05:44 -0400

    added docs to the menu item class

--- src/models/menu-item.ts
+++ src/models/menu-item.ts
@@ -1,12 +1,51 @@
 export class MenuItem {
+    /**
+     * Item's id
+     */
     public id?: string;
+
+    /**
+     * Item's title
+     */
     public title?: string;
+
+    /**
+     * Internal property used to save the original icon
+     */
     public originalIcon?: string;
+
+    /**
+     * Zmdi icon 
+     */
     public icon?: string;
+
+    /**
+     * The order in which this item should be shown
+     */
     public order?: number;
+
+    /**
+     * If this item affers navigation to a different route. This property will hold the route name
+     */
     public route?: string;
+
+    /**
+     * IF the item offers URL navigation. You should set this property to the desire path
+     */
     public url?: string;
+
+    /**
+     * Allow you to set an external navigation url
+     */
     public externalUrl?: string;
+
+    /**
+     * Children elements for this item
+     */
     public children?: MenuItem[];
+
+    /**
+     * Determine if the element should be shown as active or not
+     */
     public active?: boolean;
 }
