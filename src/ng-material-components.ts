035b33a2d6a6d1ef8352c1511163cbe67e108e8b
Orlando Quero <orlaqp@gmail.com>
2017-05-10 15:35:18 -0400

    export models

--- src/ng-material-components.ts
+++ src/ng-material-components.ts
@@ -1,4 +1,12 @@
 // Public classes.
+export { ControlWithType } from './models/control-with-type';
+export { DateRange } from './models/date-range';
+export { MenuItem } from './models/menu-item';
+export { SelectionItem } from './models/selection-item';
+export { SubmitableFormGroup } from './models/submitable-form-group';
+export { TypeEnum } from './models/type-enum';
+export { ValidationInfo } from './models/validation-info';
+
 export { SumService } from './services/sum-service';
 export { ArithmeticModule } from './modules/arithmetic-module';
 export { MaterialUserInterfaceModule } from './modules/user-interface/material-user-interface.module';
